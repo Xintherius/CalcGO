c0c9d0900f2086696688b51715a2639cc98745f9
Andres Gonzalez <andresfelipechiscagonzalez@gmail.com>
2025-01-17 13:43:22 -0600

    Update
    Update

--- main.py
+++ main.py
@@ -5,10 +5,21 @@ from models import Course
 from utils import format_percentage
 
 def main():
-    print("Welcome to CalcGO! Your grade tracking companion.")
-    # Example: Call a function from calculations
-    score = calculate_needed_score(85, 0.4, 90)
-    print(f"You need to score at least {format_percentage(score)} on your final exam.")
+    close = 1
+    print("Welcome to CalcGO! The calculator for students!\nDeveloped by: XinMedia")
+    while (close == 1):
+        #Menu
+        menu = int(input("Menu:\n1. What can this app do?\n0. Exit\n"))
+        
+        #What does the app do?
+        if (menu == 1):
+            print("CalcGO! is a basically a grade calculator for students.\nYou may be facing a situation like 'What grade do I need to get x grade as my final'\nWe, at XinMedia have been in that situation too and would like to help you make school a little less stresfful!")
+
+        elif (menu == 0):
+            print("Goodbye!")
+            close = 0
+        else:
+            print("Invalid option, please choose a number from the menu.")
 
 if __name__ == "__main__":
     main()
